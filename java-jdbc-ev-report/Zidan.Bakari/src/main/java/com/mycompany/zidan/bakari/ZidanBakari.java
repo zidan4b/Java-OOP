3c082aae77c4d13387d81198211f0164037e182c
Zidan Bakari <zidan4bakari@gmail.com>
2026-04-02 23:14:13 -0400

    Add files via upload
    java-jdbc-ev-report project

---
+++ java-jdbc-ev-report/Zidan.Bakari/src/main/java/com/mycompany/zidan/bakari/ZidanBakari.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zidan.bakari;
+
+/**
+ *
+ * @author Zidan Bakari
+ */
+public class ZidanBakari {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
